3f781bd1d1e920d10ff8fd9a5d4924f3cf340e82
ebin <ebin011@gmail.com>
2016-11-28 17:14:07 +0530

    jqueryPagination

--- includes/js/movieJquery.js
+++ includes/js/movieJquery.js
@@ -1,25 +1,41 @@
+
+//var pageNum=1;
+var itemNum=0;
+var totalPage=0;
+var total;
+var curPage;
+
+function displayMovies(itemNum,pgmNum)
+{
 $(document).ready(function(){
-			$("button").click(function(){
+	console.log(pgmNum);
+			
 				var movieList=$("#movieList");
 				var title=$("#movie").val();
 
 				$.ajax({
 					type: 'GET',
-					url: "http://www.omdbapi.com/?s="+title,
+					url: "http://www.omdbapi.com/?s="+title+"&page="+pgmNum, 
 					
 					error: function() {
 						$(movieList).html('<p>An error has occurred</p>');
 					},
 					
 					success: function(data) {
-					
+					        
 						//$.each(data,function(key, val){
 							var list=data.Search;
 							var imgPoster;
 							 $('#display').html("");
 							//var total=data.totalResults;
-							var total=data.totalResults;
-							console.log(data.totalResults);
+							 total=data.totalResults;
+							totalPage=parseInt(data.totalResults);
+							console.log(totalPage);
+							$("#pageTab").pagination('updateItems',totalPage);
+							//$("#pageTab").pagination('selectPage', curPage);
+							// $("#pageTab").pagination('getCurrentPage');
+							//$("#pageTab").pagination('nextPage');
+							
 							//$("#info").show();
 
 							/*var prop=data.Search.Year;
@@ -33,15 +49,15 @@ $(document).ready(function(){
 								return parseFloat(b.Year) - parseFloat(a.Year);
 							});
 
-
-
+							console.log(list);
+							console.log("item"+ list[itemNum].Title);
 							if (total==0)
 							{
 								$("#display").append("<b>Sorry,No data found</b>")
 							}
 							else
 							{
-								for(var i=0;i<total;i++)
+								for(var i=itemNum;i<total;i++)
 								{
 									if(list[i].Poster=="N/A")
 									{
@@ -60,7 +76,9 @@ $(document).ready(function(){
 											  +"<div class="+" panel-body "+">"+"<img class="+" img-responsive "+" width= "+" 300 "+" height= "+" 400 "
 											  +" src= " +imgPoster+ "></div>"
 											+"</div></div>");
-																				
+											$("#pageTab").attr("style","visibility:visible");
+											//pageNum=pageNum+1;	
+											//console.log("inside function"+i);					
 								/*$("#display").append("<div class="+"col-sm-6"+"><ul><li"+list[i].Title+"</li>"+
 									"<li><b>Year:</b>"+list[i].Year+"</li><br>"+
 									"<li><b>imdbID:</b>"+list[i].imdbID+"</li><br>"+
@@ -94,8 +112,65 @@ $(document).ready(function(){
 
 
 					},
+
 					
 				});
 			
-			});	
+			
 		});
+}
+
+$(function() {
+    $("#pageTab").pagination({
+        items: totalPage,
+        itemsOnPage: 10,
+        cssStyle: 'light-theme'
+    });
+});
+/*$(function() {
+    $("#pageTab").pagination('selectPage', pageNum);
+});*/
+
+
+/*$(function() {
+	console.log(totalPage);
+    $("#pageTab").pagination('updateItems',totalPage);
+});*/
+
+/*$(function() {
+    $("#pageTab").pagination('updateItemsOnPage', 10);
+});
+
+$(function() {
+    $("#pageTab").pagination('prevPage');
+});
+
+$(function() {
+    $("#pageTab").pagination('nextPage');
+});
+$(function() {
+    $(selector).pagination('getCurrentPage');
+    curPage=$("#pageTab .active ").text();
+	console.log(curPage);
+});
+$(function() {
+    $("#pageTab").pagination('enable');
+});*/
+//$('#pageTab').pagination('prevPage');
+
+//$("#pageTab").pagination('nextPage');
+$('#pageTab .next').click(function(){
+	pageNum++;
+	console.log(itemNum);
+	//itemNum+=10;
+	console.log("inside next"+itemNum)
+	displayMovies(itemNum);
+});	
+
+$('.prev').click(function(){
+	if(pageNum > 1){
+		pageNum--;
+		//itemNum-=10;
+		displayMovies(itemNum);
+	}
+	});
